10dd87166422ed0900aaa30bf161a4c956db8370
pluw3v0 <Stalker.Stalker99@mail.ru>
2021-06-26 18:52:07 +0300

    Update ViewController

--- EduTestProject/ViewController.swift
+++ EduTestProject/ViewController.swift
@@ -1,15 +1,32 @@
-//
-//  ViewController.swift
-//  EduTestProject
-//
-//  Created by Petr Novoselov on 26.06.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    override func loadView() {
+        super.loadView()
+        view = tableView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
