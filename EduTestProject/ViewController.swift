3572d9e406e199499399a4eae4e6ccbcc34fbc8f
ilmirka <shgbievi@gmail.com>
2021-06-27 10:50:45 +0300

    go apex bruh

--- EduTestProject/ViewController.swift
+++ EduTestProject/ViewController.swift
@@ -7,9 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView = UITableView()
+    
+    override func loadView() {
+        super.loadView()
+        view = tableView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
+
