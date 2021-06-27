b464c6f94f09edbb5ffa345ac17f9da67737648e
zadykian <zadykian@outlook.com>
2021-06-27 09:44:40 +0300

    ViewController.swift

--- EduTestProject/ViewController.swift
+++ EduTestProject/ViewController.swift
@@ -7,9 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let mockView = UIView()
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        view.addSubview(mockView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
